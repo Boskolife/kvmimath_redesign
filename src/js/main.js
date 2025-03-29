6fe59ca5fdc4af854e354e0a9e5fafc0b64bc41d
Galaxy Develope <94291571+Boskolife@users.noreply.github.com>
2025-03-29 17:06:29 +0200

    add: burger menu | add pacifico font

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,62 @@
-showDropDown();
+import { fadeIn, fadeOut } from './fade';
 
+showDropDown();
+initTabs();
+burgerInit();
+
+function burgerInit() {
+  const burger = document.querySelector('.header_burger');
+  const menu = document.querySelector('.header_content-bottom');
+
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('burger_active');
+    menu.classList.toggle('menu_active');
+    document.body.classList.toggle('body_lock');
+  });
+}
 
 function showDropDown() {
-  const button = document.querySelector('.dropdown-button');
-  const content = document.querySelector('.dropdown-content');
+  const buttons = document.querySelectorAll('.dropdown-button');
+  const contents = document.querySelectorAll('.dropdown-content');
 
-  button.addEventListener('click', () => {
-    content.classList.toggle('show');
-    button.classList.toggle('open');
+  buttons.forEach((button, index) => {
+    button.addEventListener('click', (event) => {
+      event.stopPropagation();
+      const isActive = contents[index].classList.contains('show');
+
+      contents.forEach((content) => content.classList.remove('show'));
+      buttons.forEach((btn) => btn.classList.remove('open'));
+
+      if (!isActive) {
+        contents[index].classList.add('show');
+        button.classList.add('open');
+      }
+    });
   });
 
-  document.addEventListener('click', (event) => {
-    if (!button.contains(event.target) && !content.contains(event.target)) {
-      content.classList.remove('show');
-      button.classList.remove('open');
-    }
+  document.addEventListener('click', () => {
+    contents.forEach((content) => content.classList.remove('show'));
+    buttons.forEach((button) => button.classList.remove('open'));
+  });
+}
+function initTabs() {
+  const buttons = document.querySelectorAll('.tab-button');
+  const contents = document.querySelectorAll('.tab-content');
+
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      const tab = button.getAttribute('data-tab');
+
+      buttons.forEach((btn) => btn.classList.remove('active'));
+      contents.forEach((content) => {
+        content.classList.remove('active');
+        content.style.opacity = 0;
+      });
+
+      button.classList.add('active');
+      const activeTab = document.getElementById(tab);
+      activeTab.classList.add('active');
+      fadeIn(activeTab, 300);
+    });
   });
 }
